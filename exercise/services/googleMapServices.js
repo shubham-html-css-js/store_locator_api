ce3f80593fa49930f2af704faa1182a50a15d66d
shubham-html-css-js <90316825+shubham-html-css-js@users.noreply.github.com>
2021-09-08 23:21:20 +0530

    Add files via upload

--- exercise/services/googleMapServices.js
+++ exercise/services/googleMapServices.js
@@ -1,1 +1,23 @@
-
+const axios=require('axios');
+let geoURL;
+class GoogleMaps
+{
+    async getCoordinates(zipCode)
+    { let coordinate=[];
+        geoURL=`https://api.mapbox.com/geocoding/v5/mapbox.places/${zipCode}.json`
+       await axios.get(geoURL,{params:{
+            access_token:`${process.env.ACCESS_TOKEN}`
+        }
+    }).then((response)=>{
+       const data=response.data;
+        coordinate=[
+           data.features[0].geometry.coordinates[0],
+           data.features[0].geometry.coordinates[1]
+       ]
+    }).catch((err)=>{
+        throw new Error(err);
+    });
+    return coordinate;
+    }
+}
+module.exports=GoogleMaps;
