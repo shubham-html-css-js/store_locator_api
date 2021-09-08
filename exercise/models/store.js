ff7a3e7fd949f823f29c5e5b83f815eeee765519
shubham-html-css-js <90316825+shubham-html-css-js@users.noreply.github.com>
2021-09-08 23:19:59 +0530

    Add files via upload

--- exercise/models/store.js
+++ exercise/models/store.js
@@ -1,1 +1,23 @@
+const mongoose=require('mongoose');
 
+const storeSchema=mongoose.Schema({
+    storeName:String,
+    phoneNumber:String,
+     address:{},
+     openStatusText:String,
+     addressLine:Array,
+     location:{
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+            required: true
+          },
+          coordinates: {
+            type: [Number],
+            required: true
+          }
+     }    
+})
+
+storeSchema.index({location:"2dsphere"});
+module.exports=mongoose.model('store',storeSchema);
